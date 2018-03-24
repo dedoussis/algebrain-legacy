46f067c01d1a943db4cc146ee29942aca76cd971
Dimitrios Dedoussis <dimitrios.dedoussis@bt.com>
2018-03-24 17:00:59 +0000

    VarNode unit test

---
+++ src/tests/VarNodeTest.java
@@ -0,0 +1,43 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import common.system.VarNode;
+
+public class VarNodeTest {
+	public static VarNode var1 = null;
+	public static VarNode var2 = null;
+
+	@Before
+	public void setUp() throws Exception {
+		var1 = new VarNode("x");
+		var2 = new VarNode("y");
+	}
+
+	@Test
+	public void testVarNode() {
+		assertTrue(var1 instanceof VarNode);
+	}
+	
+	@Test
+	public void testSetKey() {
+		var1.setKey("z");
+		assertEquals("z", var1.getKey());
+	}
+	
+	@Test
+	public void testGetKey() {
+		assertEquals("x", var1.getKey());
+	}
+	
+	@Test
+	public void testCompareTo() {
+		VarNode var3 = new VarNode("x");
+		assertTrue(var1.compareTo(var2)!=0);
+		assertEquals(0, var3.compareTo(var1));
+	}
+
+}
